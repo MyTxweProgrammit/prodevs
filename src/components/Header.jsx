b62a7a4060bbaeeaf3adf73191aaa4030d3ca2cb
humanyoungtee <humanyoungtee@gmail.com>
2026-01-06 13:08:04 +0000

    Fix Some Ugly UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,17 +11,17 @@ export default function Header() {
     }
 
     return (
-        <div className="shadow-sm w-screen h-[60px] around bg-white">
+        <div className="shadow-sm w-screen h-[60px] around bg-white sticky top-0 z-10">
             <div className="center gap-[7px]">
                 <img src={CaptureLogo} alt="logo website" class="w-[32px] h-[30px]"/>
                 <div className="center">
-                    <h3 className="text-[#495199] text-lg font-extrabold">Pro</h3>
-                    <h3 className="text-[#495199] text-lg font-medium">DEVs</h3>
+                    <h3 className="text-[#495199] text-lg font-extrabold max-[440px]:hidden">Pro</h3>
+                    <h3 className="text-[#495199] text-lg font-medium max-[440px]:hidden">DEVs</h3>
                 </div>
             </div>
             <div className="center gap-[10px]">
-                <div className="text-[#495199] font-medium rounded-lg py-[3px] px-[8px] duration-[0.6s] cursor-pointer hover:bg-[#94bdff] hover:text-white">Login</div>
-                <div className="text-white font-medium rounded-lg py-[3px] px-[8px] duration-[0.5s] cursor-pointer bg-[#003c9c] hover:bg-[#0062ff]">Get Started</div>
+                <div className="text-[#495199] font-medium rounded-lg py-[3px] px-[8px] duration-[0.6s] cursor-pointer hover:bg-[#94bdff] hover:text-white active:bg-[#94bdff] active:text-white">Login</div>
+                <div className="text-white font-medium rounded-lg py-[3px] px-[8px] duration-[0.5s] cursor-pointer bg-[#003c9c] hover:bg-[#0062ff] active:bg-[#0062ff]">Get Started</div>
                 <div 
                     onMouseEnter={TrueAndChange} 
                     onMouseLeave={TrueAndChange} 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,19 +7,22 @@ export default function Main() {
         <div className="w-screen h-screen">
             <title>ProDEVs - Learn Everything About Programming!</title>
             <Header />
-            <div className="group bg-white w-screen bg-white pt-[10%]">
+            <div className="bg-white w-screen bg-white pt-[10%] pb-[25px] max-[450px]:pt-[80px]">
                 <div className="center"> {/* Logo */}
                     <img src={CaptureLogo} /> 
                 </div>
                 <p className="text-center bg-conic-180 from-indigo-600 via-indigo-50 to-indigo-600 text-transparent bg-clip-text text-[50px] font-medium max-[520px]:text-[35px]">ProDEVs</p>
                 <p className="text-black text-center text-[26px] font-medium max-[520px]:text-[20px]">The area of code learning that never stops!</p>
-                <div className="mt-[10px] mb-[20px] py-2 px-[10px] rounded-[25px] max-[520px]:text-[11px] w-fit text-white bg-[#5482ff] font-medium mx-auto cursor-pointer duration-[0.5s] group-hover:bg-white group-hover:border-2 group-hover:border-solid group-hover:border-[#5482ff] group-hover:text-[#5482ff] relative overflow-hidden
-                active:bg-white active:border-2 active:border-solid active:border-[#5482ff] active:text-[#5482ff]
-                ">
-                    Start Learning
-                    <div className="absolute bg-[#5482ff] w-[30px] h-full top-0 translate-x-[-30px] group-hover:translate-x-[115px] duration-[0.5s]"></div>    
+                <div className="mx-auto w-fit h-fit mt-[10px] group">
+                    <div className="py-2 px-[10px] rounded-[25px] max-[520px]:text-[11px] w-fit text-white bg-[#5482ff] font-medium cursor-pointer duration-[0.5s] group-hover:bg-white group-hover:border-2 group-hover:border-solid group-hover:border-[#5482ff] group-hover:text-[#5482ff] relative overflow-hidden
+                    active:bg-white active:border-2 active:border-solid active:border-[#5482ff] active:text-[#5482ff]
+                    ">
+                        Start Learning
+                        <div className="absolute bg-[#5482ff] w-[30px] h-full top-0 translate-x-[-40px] group-hover:translate-x-[115px] duration-[0.5s]"></div>    
+                    </div>
                 </div>
             </div>
+            <div className="bg-gradient-left w-screen h-[500px]"></div>
         </div>
     )
 }
