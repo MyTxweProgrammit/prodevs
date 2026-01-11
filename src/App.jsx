47913d7ed7d5249051f50e1db1a0c6ae2c89fd83
humanyoungtee <humanyoungtee@gmail.com>
2026-01-11 03:34:25 +0000

    Login Page Complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const login = () => { setIsAuthenticated(true) }
   const logout = () => { setIsAuthenticated(false) }
+  
   return (
     <BrowserRouter>
       <Routes>
